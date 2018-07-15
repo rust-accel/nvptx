b3420dad660e7f75502986a48b4a3c4389f7cda3
Toshiki Teramura <toshiki.teramura@gmail.com>
2018-07-16 03:45:54 +0900

    Rename to cargo-nvbuild

--- src/bin/cargo-nvptx.rs
+++ src/bin/cargo-nvptx.rs
@@ -1,10 +1,24 @@
 extern crate nvptx;
+#[macro_use]
+extern crate structopt;
 
 use nvptx::error::Result;
 use nvptx::Driver;
 
 use std::env;
 use std::path::*;
+use structopt::StructOpt;
+
+#[derive(StructOpt, Debug)]
+#[structopt(bin_name = "cargo")]
+enum Opt {
+    #[structopt(name = "nvbuild")]
+    Build {
+        /// Release build
+        #[structopt(long = "release")]
+        release: bool,
+    },
+}
 
 fn get_manifest_path() -> PathBuf {
     let mut dir = env::current_dir().unwrap();
@@ -21,6 +35,8 @@ fn get_manifest_path() -> PathBuf {
 }
 
 fn main() -> Result<()> {
+    let opt = Opt::from_args();
+    println!("{:?}", opt);
     let manifest_path = get_manifest_path();
     let ptx = Driver::with_path(manifest_path)?.compile()?;
     println!("{}", ptx);
