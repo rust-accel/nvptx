cc42f5cecc3a35e40318d2fd5ff9fd647eb0b2e3
Toshiki Teramura <toshiki.teramura@gmail.com>
2018-07-15 23:21:56 +0900

    Add cargo

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,16 @@
+//! Compile Rust into PTX string using LLVM
 
-extern crate dirs;
-extern crate glob;
 #[macro_use]
 extern crate derive_more;
-extern crate serde;
 #[macro_use]
 extern crate serde_derive;
+extern crate serde;
+extern crate dirs;
+extern crate glob;
 extern crate tempdir;
 extern crate toml;
+extern crate failure;
+extern crate cargo;
 
 pub mod config;
 pub mod compile;
-
